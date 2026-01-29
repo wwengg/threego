5f1b6676c006e1c75635790d9b52f55da9705dad
wwengg <info@wwengg.cn>
2026-01-29 19:23:00 +0800

    feat: 允许不记录敏感返回值

--- core/plugin/record.go
+++ core/plugin/record.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/smallnest/rpcx/share"
 	"github.com/wwengg/threego/core/slog"
 	"go.uber.org/zap"
 )
@@ -24,6 +25,14 @@ func (p *RecordPlugin) PreCall(ctx context.Context, serviceName, methodName stri
 }
 
 func (p *RecordPlugin) PostCall(ctx context.Context, serviceName, methodName string, args, reply interface{}, err error) (interface{}, error) {
-	p.logger.Info(fmt.Sprintf("Finish[%s][%s]", serviceName, methodName), zap.Any("reply", reply))
+	md := ctx.Value(share.ResMetaDataKey)
+	if md == nil {
+		p.logger.Info(fmt.Sprintf("Finish[%s][%s]", serviceName, methodName), zap.Any("reply", reply))
+		return reply, nil
+	}
+	if v, ok := md.(map[string]string)["record"]; ok && v == "1" {
+		// 允许敏感返回值不记录
+		p.logger.Info(fmt.Sprintf("Finish[%s][%s]", serviceName, methodName), zap.Any("reply", reply))
+	}
 	return reply, nil
 }
